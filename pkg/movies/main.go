4dcfe6e0afbb93009da74423318caba65445273d
lukegriffith <lukemgriffith@gmail.com>
2023-10-19 19:45:55 +0100

    fmt

--- pkg/movies/main.go
+++ pkg/movies/main.go
@@ -6,19 +6,16 @@ import (
 )
 
 var (
-	moviesList []Movie
-
+	moviesList              []Movie
 	MovieTitleValidationErr = errors.New("Movie title invalid")
 )
 
 type Title string
 
 func NewTitle(title string) (error, Title) {
-
 	if len(title) <= 0 {
 		return MovieTitleValidationErr, ""
 	}
-
 	return nil, Title(title)
 }
 
@@ -35,17 +32,14 @@ func init() {
 }
 
 func NewMovie(req MovieRequestProps) (error, Movie) {
-
 	err, title := NewTitle(req.Title)
 	if err != nil {
 		return fmt.Errorf("new movie error: %w", err), Movie{}
 	}
-
 	return nil, Movie{title}
 }
 
 func AddMovie(req MovieRequestProps) error {
-
 	err, movie := NewMovie(req)
 	if err != nil {
 		return fmt.Errorf("Add movie error: %w", err)
